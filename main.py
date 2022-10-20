38fdf53eef667236655522640784184b4e429f2f
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-10-19 21:18:49 -0400

    got to a good place, just need to find out how I can get the cpu to only ocnsider its availble words.

--- main.py
+++ main.py
@@ -22,12 +22,10 @@ def clear_screen() -> None:
 def main() -> None:
     game: Game = Game()
     total_moves: int = 0
-    winner: str = ""
     game_over: bool = False
     while not game_over:
         for player in game.players:
             print("\n" + str(game.board))
-
             other_player = [p for p in game.players if p != player][0]
             if isinstance(player, User):
                 word: str = get_word(p=player, g=game, op= other_player, tot_moves=total_moves)
@@ -36,6 +34,9 @@ def main() -> None:
                 index: int = get_index()
                 place_word(index=index, w=word, g=game)
             else:  # else if it is the CPU ...
+                word: str = random.choice(list(game.get_words().keys()))
+                if total_moves == 1:
+                    player.claim_letter(word[1])
                 best_move, best_score = get_best_move(game.board, game.get_words(), player, total_moves)
                 word: str = random.choice(list(game.get_words().keys()))
                 place_word(index=best_move, g=game, w=word)
@@ -43,12 +44,12 @@ def main() -> None:
             clear_screen()
             total_moves += 1
             p, won = someone_won(b=game.get_board(), player_that_just_played=player)
+            print(p.name)
             if won:
                 print(f"{p.name} Wins!")
                 game_over: bool = True
 
     # print stats like wins and stuff
-
 
 if __name__ == "__main__":
     main()
